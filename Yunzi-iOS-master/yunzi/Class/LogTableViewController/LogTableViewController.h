b0852782607139a3983684c11f989d765f3fb66c
fmchan <formingchan@gmail.com>
2015-07-16 22:04:50 +0800

    add log controller
    test

---
+++ Yunzi-iOS-master/yunzi/Class/LogTableViewController/LogTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  LogTableViewController.h
+//  yunzi
+//
+//  Created by For Ming Chan on 12/7/15.
+//  Copyright (c) 2015 sensoro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Config.h"
+#import "AppDelegate.h"
+
+@interface LogTableViewController : UITableViewController
+
+@end
